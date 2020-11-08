13c5634b21560fc19136414dc7676b189cff3cf7
staryk <staryk180289@gmail.com>
2020-11-08 17:16:42 +0300

    Add autogenerated crud for category

--- migrations/Version20201108141026.php
+++ migrations/Version20201108141026.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20200628105919 extends AbstractMigration
+final class Version20201108141026 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -16,18 +16,17 @@ final class Version20200628105919 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $this->addSql('CREATE TABLE users (
+        $this->addSql('CREATE TABLE categories (
                 id INT AUTO_INCREMENT NOT NULL,
-                email VARCHAR(255) NOT NULL,
-                password VARCHAR(60) NOT NULL,
-                PRIMARY KEY(id),
-                UNIQUE KEY users_email(email)
+                name VARCHAR(255) NOT NULL,
+                gender TINYINT NOT NULL,
+                PRIMARY KEY(id)
             ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
         );
     }
 
     public function down(Schema $schema) : void
     {
-        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE categories');
     }
 }
---
+++ src/Entity/Category.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    private const GENDER_MALE = 1;
+    private const GENDER_FEMALE = 2;
+
+    /**
+     * @var int
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(name="id", type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="gender", type="integer")
+     */
+    private $gender;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
